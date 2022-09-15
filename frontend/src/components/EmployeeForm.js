40ecacea21428cf9f178daaff2a87e9e682722f2
fran <fran.safranec95@gmail.com>
2022-09-15 21:43:46 +0100

    added employee table and added employee form

--- frontend/src/components/EmployeeForm.js
+++ frontend/src/components/EmployeeForm.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import axios from 'axios';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
 
 const EmployeeForm = () => {
     return (
         <>
-            <p>EmployeeForm</p>
+            <Card style={{ padding: '2rem' }}>
+                <Card.Title style={{ paddingBottom: '1rem' }}>Create / Update employee</Card.Title>
+                <Form>
+                    <Form.Group className="mb-3">
+                        <Form.Label>First Name</Form.Label>
+                        <Form.Control type="text" placeholder="Enter First Name" />
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Last Name</Form.Label>
+                        <Form.Control type="text" placeholder="Enter Last Name" />
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email" placeholder="Enter Email" />
+                    </Form.Group>
+                </Form>
+            </Card>
         </>
     );
 }
--- frontend/src/components/EmployeeTable.js
+++ frontend/src/components/EmployeeTable.js
@@ -1,42 +1,63 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
+import Placeholder from 'react-bootstrap/Placeholder';
 import axios from 'axios';
 
-const fetchEmployees = async () => {
-    const empyloees = await axios.get('http://localhost:8080/api/employees')
-    console.log(empyloees);
-}
+const EmployeeTable = () => {
 
-const renderTable = () => {
+    const [employees, setEmployees] = useState([]);
+
+    const fetchEmployees = async () => {
+        const employeesList = await axios.get('http://localhost:8080/api/employees')
+        setEmployees(employeesList.data.data.employees)
+    }
+
+    const deleteEmployee = async (employeeId) => {
+        await axios.delete(`http://localhost:8080/api/employee/${employeeId}`)
+    }
+
+    useEffect(() => {
+        fetchEmployees()
+    }, [])
+
     return (
         <>
-            <Table striped>
-                <thead>
-                    <tr>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>email</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        <td><Button variant='warning' size='sm' onClick={fetchEmployees}>Update</Button></td>
-                        <td><Button variant='danger' size='sm'>Delete</Button></td>
-                    </tr>
-
-                </tbody>
-            </Table>
+            {!employees?.length ?
+                <Placeholder as="p" animation="glow">
+                    <Placeholder xs={12} />
+                    <Placeholder xs={12} />
+                    <Placeholder xs={12} />
+                    <Placeholder xs={12} />
+                    <Placeholder xs={12} />
+                    <Placeholder xs={12} />
+                    <h5>Is your backend running ? 😥</h5>
+                </Placeholder>
+                :
+                <Table striped>
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {employees.map(employee => (
+                            <tr key={employee._id}>
+                                <td>{employee._id}</td>
+                                <td>{employee.firstName}</td>
+                                <td>{employee.lastName}</td>
+                                <td>{employee.email}</td>
+                                <td><Button variant='warning' size='sm'>Update</Button></td>
+                                <td><Button variant='danger' size='sm' onClick={() => deleteEmployee(employee._id)}>Delete</Button></td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+            }
         </>
-    );
-}
-
-const EmployeeTable = () => {
-    return (
-        renderTable()
     );
 }
 
